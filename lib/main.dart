60976e16d8958928696c1998ec120cc90358a46e
Prajwal <abc>
2022-11-12 20:32:28 +0530

    Login and Signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:the_news_app/screens/navigation_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyB3t1bCZmxjbgM6BMtpFk-l1Wamsw6MjKs",
+            projectId: "auth-and-login-55704",
+            messagingSenderId: "289798417709",
+            appId: "1:289798417709:android:50f9c365c95cd1777cb7a4"));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
